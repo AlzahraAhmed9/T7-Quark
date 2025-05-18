04fbd41874821a18e6afaf85cef18794b8e24fd2
AlzahraAhmed9 <alzahra.alberiki@gmail.com>
2025-05-18 12:56:43 +0400

    Addiction added!

--- calculator.py
+++ calculator.py
@@ -1,3 +1,8 @@
-number_1= int(input("Enter the 1st  number ^-^ : "))
-number_2= int(input("Enter the 1st  number ^-^ : "))
+number_1= input("Enter the 1st  number ^-^ : ")
+number_2= input("Enter the 1st  number ^-^ : ")
+operation=input("Enetr the operation *-* : ")
 
+
+# The operations :
+if operation=="+":
+    result= int(number_1) + int(number_2) 
