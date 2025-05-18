5a746c92729ef3a228d06d05486e7ba6281416e7
khadija829 <khadijazad313@gmail.com>
2025-05-18 13:36:12 +0400

    Changes where added!

--- calculator.py
+++ calculator.py
@@ -1,8 +1,20 @@
 number_1= input("Enter the 1st  number ^-^ : ")
 number_2= input("Enter the 1st  number ^-^ : ")
 operation=input("Enetr the operation *-* : ")
+result = 0 
 
 
 # The operations :
 if operation=="+":
     result= int(number_1) + int(number_2) 
+
+elif operation=="- ":
+    result= int(number_1) - int(number_2)
+
+elif operation=="/":
+    result= int(number_1) / int(number_2)
+
+
+
+# output
+print("Result is:", result )
